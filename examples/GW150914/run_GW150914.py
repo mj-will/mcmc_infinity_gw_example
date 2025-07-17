fc081f13ee74510005ac47e829d5408615ba3742
Michael J. Williams <michaeljw1@googlemail.com>
2025-07-17 09:06:32 +0100

    add basic run script

---
+++ examples/GW150914/run_GW150914.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+"""
+Analyse GW150914 using bilby and nessai.
+
+Uses IMRPhenomD waveform approximant and a prior defined in
+GW150914.prior. 
+"""
+import bilby
+from gwpy.timeseries import TimeSeries
+import os
+logger = bilby.core.utils.logger
+
+outdir = "outdir"
+label = "GW150914_nessai"
+
+trigger_time = 1126259462.4
+detectors = ["H1", "L1"]
+maximum_frequency = 512
+minimum_frequency = 20
+roll_off = 0.4  # Roll off duration of tukey window in seconds, default is 0.4s
+duration = 4  # Analysis segment duration
+post_trigger_duration = 2  # Time between trigger time and end of segment
+end_time = trigger_time + post_trigger_duration
+start_time = end_time - duration
+
+psd_duration = 32 * duration
+psd_start_time = start_time - psd_duration
+psd_end_time = start_time
+
+ifo_list = bilby.gw.detector.InterferometerList([])
+for det in detectors:
+    logger.info("Downloading analysis data for ifo {}".format(det))
+    ifo = bilby.gw.detector.get_empty_interferometer(det)
+    data = TimeSeries.fetch_open_data(det, start_time, end_time, cache=True)
+    ifo.strain_data.set_from_gwpy_timeseries(data)
+
+    logger.info("Downloading psd data for ifo {}".format(det))
+    psd_data = TimeSeries.fetch_open_data(det, psd_start_time, psd_end_time)
+    psd_alpha = 2 * roll_off / duration
+    psd = psd_data.psd(
+        fftlength=duration, overlap=0, window=("tukey", psd_alpha), method="median"
+    )
+    ifo.power_spectral_density = bilby.gw.detector.PowerSpectralDensity(
+        frequency_array=psd.frequencies.value, psd_array=psd.value
+    )
+    ifo.maximum_frequency = maximum_frequency
+    ifo.minimum_frequency = minimum_frequency
+    ifo_list.append(ifo)
+
+logger.info("Saving data plots to {}".format(outdir))
+bilby.core.utils.check_directory_exists_and_if_not_mkdir(outdir)
+ifo_list.plot_data(outdir=outdir, label=label)
+
+priors = bilby.gw.prior.BBHPriorDict(filename="GW150914.prior")
+
+priors["geocent_time"] = bilby.core.prior.Uniform(
+    trigger_time - 0.1, trigger_time + 0.1, name="geocent_time"
+)
+
+waveform_generator = bilby.gw.WaveformGenerator(
+    frequency_domain_source_model=bilby.gw.source.lal_binary_black_hole,
+    parameter_conversion=bilby.gw.conversion.convert_to_lal_binary_black_hole_parameters,
+    waveform_arguments={
+        "waveform_approximant": "IMRPhenomD",
+        "reference_frequency": 50,
+    },
+)
+
+likelihood = bilby.gw.likelihood.GravitationalWaveTransient(
+    ifo_list,
+    waveform_generator,
+    priors=priors,
+    time_marginalization=False,
+    phase_marginalization=True,
+    distance_marginalization=True,
+)
+
+
+if __name__ == "__main__":
+    os.environ["OMP_NUM_THREADS"] = "1"  # Set number of threads to 1 
+
+    result = bilby.run_sampler(
+        likelihood,
+        priors,
+        sampler="nessai",
+        outdir=outdir,
+        label=label,
+        nlive=1000,
+        npool=4,
+        conversion_function=bilby.gw.conversion.generate_all_bbh_parameters,
+        result_class=bilby.gw.result.CBCResult,
+    )
+    result.plot_corner()
